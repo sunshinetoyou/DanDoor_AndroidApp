3b1f66dfc876866912b60d5dab142e69853981a7
Yeonsu Jung <victorr0618@naver.com>
2025-07-02 14:02:17 +0900

    권한 추가 및 스캔 데이터 생성
    어노테이션 사용을 위한 추가 권한 설정과 저장할 데이터 형식(ScanData)을 생성

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
