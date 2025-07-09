6c145e535dbec5f08942a89f143eb14e26de1222
Yeonsu Jung <victorr0618@naver.com>
2025-07-09 13:49:44 +0900

    Update Kotlin version and library metadata
    Bump Kotlin to 2.1.21 and update JVM target to 17. Change artifactId to DanDoor_AndroidApp and version to 0.0.3 in build.gradle.kts. Update dependency to use the new version.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     publishing {
         singleVariant("release") {
@@ -45,8 +45,8 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 from(components["release"])
                 groupId = "com.github.sunshinetoyou"
-                artifactId = "com.dandoor.ddlib"
-                version = "0.0.2" // 예: "1.0.0"
+                artifactId = "DanDoor_AndroidApp"
+                version = "0.0.3" // 예: "1.0.0"
             }
         }
     }
@@ -63,5 +63,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.github.sunshinetoyou:DanDoor_AndroidApp:0.0.2")
+    implementation("com.github.sunshinetoyou:DanDoor_AndroidApp:0.0.3")
 }
